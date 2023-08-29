68e40db2aecbc410062c1dd8b6472a976c80f321
Js-Erick <103290897+Js-Erick@users.noreply.github.com>
2023-08-28 21:34:15 -0400

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,8 +5,39 @@ import { Component } from '@angular/core';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
+
+  private _fechaActual: string = new Date().toISOString();
+  public diasParaSumar: number | null = null;
+  public fechaFutura: Date | null = null;
+
+  get fechaActual(): string {
+    return this._fechaActual;
+  }
+
+  set fechaActual(value: string) {
+    this._fechaActual = value;
+    this.resetFechaFuturaOnChange();
+  }
 
   constructor() {}
 
+  calcularFechaFutura() {
+    if (this._fechaActual && this.diasParaSumar !== null) {
+      let fecha = new Date(this._fechaActual);
+      fecha.setDate(fecha.getDate() + this.diasParaSumar);
+      this.fechaFutura = fecha;
+    }
+  }
+
+  resetFechaFuturaOnChange() {
+    this.fechaFutura = null;
+  }
+
+  resetFechaFuturaOnInput() {
+    if (!this.diasParaSumar) {
+      this.fechaFutura = null;
+    }
+  }
 }
